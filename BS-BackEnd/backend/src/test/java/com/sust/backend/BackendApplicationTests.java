2e13947ea4c64436903c63f61204e642e4e27347
forcebo <1974226194@qq.com>
2023-05-15 21:07:25 +0800

    后端初始化搭建成功

---
+++ BS-BackEnd/backend/src/test/java/com/sust/backend/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sust.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("搭建成功");
+    }
+
+}
