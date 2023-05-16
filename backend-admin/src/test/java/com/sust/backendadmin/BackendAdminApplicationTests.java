130f37e4d84e0bc66040efa288abe5ffc99e9457
Shiona <xapenny@icloud.com>
2023-05-16 15:58:13 +0800

    Fixed project structure

---
+++ backend-admin/src/test/java/com/sust/backendadmin/BackendAdminApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sust.backendadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("123");
+	}
+
+}
