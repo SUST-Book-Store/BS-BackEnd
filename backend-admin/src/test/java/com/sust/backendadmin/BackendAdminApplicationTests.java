2e6aa27123d706a87467e084a8e87fb2b102725a
forcebo <1974226194@qq.com>
2023-05-21 18:18:52 +0800

    配置数据库信息，引入依赖和实现获取书本接口

--- backend-admin/src/test/java/com/sust/backendadmin/BackendAdminApplicationTests.java
+++ backend-admin/src/test/java/com/sust/backendadmin/BackendAdminApplicationTests.java
@@ -1,14 +1,24 @@
 package com.sust.backendadmin;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.sust.backendadmin.mapper.BookMapper;
+import com.sust.backendadmin.pojo.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class BackendAdminApplicationTests {
+	@Autowired
+	private BookMapper bookMapper;
 
 	@Test
 	void contextLoads() {
-		System.out.println("Test123");
+		QueryWrapper<Book> queryWrapper = new QueryWrapper<>();
+		List<Book> books = bookMapper.selectList(queryWrapper);
+		System.out.println(books);
 	}
 
 }
