cdaf09b425c0da209cd1a5418d8f424417842775
Shiona <xapenny@icloud.com>
2023-05-28 10:39:58 +0800

    新增商品搜索

--- backend-admin/src/main/java/com/sust/backendadmin/controller/book/BookController.java
+++ backend-admin/src/main/java/com/sust/backendadmin/controller/book/BookController.java
@@ -1,6 +1,7 @@
 package com.sust.backendadmin.controller.book;
 
 import cn.hutool.core.util.StrUtil;
+import com.sust.backendadmin.dto.SearchBooksDto;
 import com.sust.backendadmin.pojo.Result;
 import com.sust.backendadmin.service.book.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,20 @@ public class BookController {
         Integer pageNum =Integer.parseInt(data.get("pageNum"));
         Integer pageSize = Integer.parseInt(data.get("pageSize"));
         return Result.ok(bookService.getPage(pageNum, pageSize));
+    }
+
+    @GetMapping("/search")
+    public Result searchBook(@RequestParam Map<String, String> data) {
+        if (StrUtil.isBlank(data.get("keyword")) || StrUtil.isBlank(data.get("page"))) {
+            return Result.fail("传入参数有误");
+        } else {
+            int page = Integer.parseInt(data.get("page"));
+            SearchBooksDto searchBooksDto = new SearchBooksDto();
+            searchBooksDto.setName("%"+data.get("keyword")+"%");
+            searchBooksDto.setPageNum(page);
+            searchBooksDto.setPageSize(10);
+            return bookService.lists(searchBooksDto);
+        }
     }
 
     @GetMapping("/detail/{id}")
